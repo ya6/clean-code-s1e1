2a8af536639567ddf79993ad844113fd6f263290
YA <vector_cp@mail.ru>
2022-06-05 00:05:14 +0300

    refactor: add class ident to img

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn btn_delete"; 
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='remove-img';
     deleteButtonImg.setAttribute('alt', 'remove');
     deleteButton.appendChild(deleteButtonImg);
 
